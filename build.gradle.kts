a3c14c5363b8e8524954999e30d22a46561fa53d
Redish03 <pluto0303@pukyong.ac.kr>
2026-04-16 13:48:38 +0900

    chore: gradle에 spring, h2 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.3.0"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
+    id("org.springframework.boot") version "4.0.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "camp.nextstep.edu"
@@ -18,6 +20,10 @@ dependencies {
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.14.3")
     testImplementation("org.assertj", "assertj-core", "3.27.7")
     testImplementation("io.kotest", "kotest-runner-junit5", "6.0.7")
+    runtimeOnly("com.h2database:h2:2.4.240")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 tasks {
