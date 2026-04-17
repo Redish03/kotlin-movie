e801577591ed2c01fb55365807aeb1ea548cae71
Redish03 <pluto0303@pukyong.ac.kr>
2026-04-17 12:24:18 +0900

    feat: API Controller 확인

--- src/main/kotlin/movie/Application.kt
+++ src/main/kotlin/movie/Application.kt
@@ -2,9 +2,23 @@ package movie
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
 
 @SpringBootApplication
 class Application
+
+data class Greeting(val id: Long, val content: String)
+
+@RestController
+class GreetingController {
+    private val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam name: String = "World") = Greeting(counter.incrementAndGet(), name)
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
